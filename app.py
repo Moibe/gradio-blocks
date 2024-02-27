af7e87c15bd6b62e93ba7f5e80d0eeae52454e54
Moibe <moi_estrello@hotmail.com>
2024-02-27 16:39:59 -0600

    Remove transformers

--- app.py
+++ app.py
@@ -2,15 +2,31 @@ import gradio as gr
 
 from transformers import pipeline
 
-pipe = pipeline("translation", model="t5-base")
+#pipe = pipeline("translation", model="t5-base")
+
+get_local_storage = """
+    function() {
+      globalThis.setStorage = (key, value)=>{
+        localStorage.setItem(key, JSON.stringify(value))
+      }
+       globalThis.getStorage = (key, value)=>{
+        return JSON.parse(localStorage.getItem(key))
+      }
+       const text_input =  getStorage('text_input')
+       return text_input;
+      }
+    """
+
 
 def translate(text):
-    return pipe(text)[0]["translation_text"]
+    return text
 
 with gr.Blocks() as demo:
     with gr.Row(): 
         with gr.Column():
             english = gr.Textbox(label="English text")
+            text_input = gr.Text(label="Input va allá")
+            text_input.change(None, text_input, None, _js="(v)=>{ setStorage('text_input',v) }")
             translate_btn = gr.Button(value="Translate")
         with gr.Column():
             german = gr.Textbox(label="German Text")
@@ -19,4 +35,10 @@ with gr.Blocks() as demo:
     examples = gr.Examples(examples=["I went to the supermarket yesterday.", "Helen is a good swimmer."],
                         inputs=[english])
 
-demo.launch(share=True)
+demo.load(
+        None,
+        inputs=None,
+        outputs=text_input,
+        _js=get_local_storage,
+    )
+demo.launch(debug=True)
