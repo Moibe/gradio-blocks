8d70c8bb4a3cc5cb0f25081e053b21f8a7116895
Moibe <moi_estrello@hotmail.com>
2024-02-27 18:41:04 -0600

    Gradio label added

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def predict(text_input, dropdown):
 
 
 with gr.Blocks() as block:
+    tokens_label = gr.Label("Etiqueta")
     text_input = gr.Text(label="Input183")
     dropdown = gr.Dropdown(["first", "second", "third"], type="index")
     local_data = gr.JSON({}, label="Local Storage")
