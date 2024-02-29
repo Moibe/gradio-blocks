9deaf7417c89b975b0905f51f6aa6035fe2c7627
Moibe <moi_estrello@hotmail.com>
2024-02-29 04:06:36 -0600

    Condiciones para uso de tokens

--- app.py
+++ app.py
@@ -11,11 +11,61 @@ get_local_storage = """
         return obtencion
       }
 
+       globalThis.cleanCred = (key, value)=>{
+        localStorage.setItem(key, value)
+      }
+
+      //El token se deberá obtener de todas formas: 
+      // Obtener la URL actual
+        const urlActual = window.location;
+        console.log("Hola")
+        console.log(urlActual)
+
+        // Buscar el parámetro "tkn"
+        const urlParams = new URLSearchParams(window.location.search);
+        console.log("Ésto es urlParams: ", urlParams)
+
+        const token = urlParams.get('tkn');
+
+        console.log("El token enviado es: ", token)
+
        if (!localStorage.getItem('tokens')) {
+        console.log("Primera vez en el sistema, se le dan 5 tokens")
         localStorage.setItem('tokens', 5);
+        //Para llegar a éste punto significa que no habían entrado sin embargo traían tokens, lo cual no se puede y está trucado.
+        if(token){ 
+            localStorage.setItem('trp', 'Ok')
+            localStorage.setItem('tokens', 0)
+            console.log("El usuario cargo la url sin haber usado el sitio, queda flagged como trampa y se le otorgan 0 tokens.")
+            }
+        }
+        else
+        {
+            console.log("Ya tiene hecho el espacio para tokens, no es la primera vez que visita. Si podemos recargar, solo aquí:")
+            console.log("token es: ", token)
+            trap = localStorage.getItem('trp') 
+            console.log("trap es: ", trap)
+            credused = localStorage.getItem('credused')
+            console.log("credused es: ", credused)
+
+        if (token && trap === null && credused === null || token && trap === null && credused === "0") {
+            console.log('Token encontrado hoy:', token);
+            tokens_actuales = localStorage.getItem('tokens');
+            console.log("Los tokens actuales que tienes son: ", tokens_actuales)
+            nueva_cantidad = parseInt(tokens_actuales) + parseInt(token)
+            localStorage.setItem('tokens', nueva_cantidad);
+            localStorage.setItem('credused', 1)
+            console.log("El usuario cargo sus tokens, se marca que ya usó ese crédito.")
+        } 
+        else { console.log('No se encontró el token'); }
+
+
         }
 
+        //Al final obten cuantos tokens tiene.
         const legado = localStorage.getItem('tokens');
+
+        
 
         return legado;
        
@@ -29,14 +79,14 @@ def predict(text_input, tokens_label):
     print("Tokens_Label:", tokens_label)
 
     if tokens_texto > 0:
-        
         print("Saldo Positivo")
-        return tokens_texto, resultado_texto, gr.Button(interactive=True)
+        return tokens_texto, resultado_texto, gr.Button(interactive=True), gr.Button(visible=False)
 
     else:
     
         print("Saldo negativo")
-        return tokens_texto, resultado_texto, gr.Button(interactive=False)
+        return tokens_texto, resultado_texto, gr.Button(interactive=False), gr.Button(visible=True)
+
 
 with gr.Blocks() as block:
 
@@ -49,7 +99,12 @@ with gr.Blocks() as block:
 
     #resultadoFinal.change(None, text_input, resultadoFinal, js="(v)=>{ getStorage('text_input') }")
     btn = gr.Button("Enviar", icon="aiicon.png", interactive = True)
-    btn.click(fn=predict, inputs=[text_input, tokens_label], outputs=[tokens_label, resultadoFinal, btn])
+    payBtn = gr.Button("Buy Tokens", icon="aiicon.png", interactive = True, visible = False, link="https://google.com")
+
+    btn.click(fn=predict, inputs=[text_input, tokens_label], outputs=[tokens_label, resultadoFinal, btn, payBtn])
+    payBtn.click(None, inputs=None, outputs=None, js="(v)=>{ cleanCred('credused',0) }" )
+
+    
  
     block.load(
         None,
@@ -57,4 +112,5 @@ with gr.Blocks() as block:
         outputs=[tokens_label],
         js=get_local_storage,
     )
+
 block.launch(debug=True)
