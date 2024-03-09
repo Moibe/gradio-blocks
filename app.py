d24eab729ba7572f454ba6a791d1003d3821771a
Moibe <moi_estrello@hotmail.com>
2024-03-08 19:04:33 -0600

    Interactive 9

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ with gr.Blocks() as block:
     resultadoFinal = gr.Text(label="Resultado")
     
     #text_input.change(None, tokens_label, tokens_label, js="(v)=>{ getStorage('text_input',v) }")
-    tokens_label.change(None, tokens_label, None, _js="(v)=>{ setStorage('tokens',v) }")
+    tokens_label.change(None, tokens_label, None, _js="(v)=>{ setStorage('tokens',9) }")
 
     #resultadoFinal.change(None, text_input, resultadoFinal, js="(v)=>{ getStorage('text_input') }")
     btn = gr.Button("Enviar", icon="aiicon.png")
