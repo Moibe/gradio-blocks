f8c3a1f14d6fe336b1654de2dd1d455ff0e446e4
Moibe <moi_estrello@hotmail.com>
2024-03-08 19:29:32 -0600

    Temp remove tokens_label change

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ with gr.Blocks() as block:
     resultadoFinal = gr.Text(label="Resultado")
     
     #text_input.change(None, tokens_label, tokens_label, js="(v)=>{ getStorage('text_input',v) }")
-    tokens_label.change(None, tokens_label, None, _js="(v)=>{ setStorage('otro',v) }")
+    #tokens_label.change(None, tokens_label, None, _js="(v)=>{ setStorage('otro',v) }")
     resultadoFinal.change(None, tokens_label, None, _js="(v)=>{ setStorage('tokens',88) }")
 
     #resultadoFinal.change(None, text_input, resultadoFinal, js="(v)=>{ getStorage('text_input') }")
