cf804add12dc3862e10a41f5def2eccf14bbf84a
Moibe <moi_estrello@hotmail.com>
2024-03-08 20:18:06 -0600

    Step 1 writestorage when result change

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ with gr.Blocks() as block:
 
     #text_input.change(None, tokens_label, tokens_label, js="(v)=>{ getStorage('text_input',v) }")
     #tokens_label.change(None, tokens_label, None, _js="(v)=>{ setStorage('otro',v) }")
-    resultadoFinal.change(None, text_input, None, _js="(v)=>{ setStorage('tokens', v) }")
+    resultadoFinal.change(None, text_input, None, _js="(v)=>{ setStorage('tokens', 122) }")
 
     #resultadoFinal.change(None, text_input, resultadoFinal, js="(v)=>{ getStorage('text_input') }")
     btn = gr.Button("Enviar", icon="aiicon.png")
