2e89a7db97b9e3a5d0c38e4027a899784011e9be
Moibe <moi_estrello@hotmail.com>
2024-03-08 22:46:18 -0600

    Move place resultadoFinal.change

--- app.py
+++ app.py
@@ -104,13 +104,14 @@ with gr.Blocks() as block:
 
     #text_input.change(None, tokens_label, tokens_label, js="(v)=>{ getStorage('text_input',v) }")
     #tokens_label.change(None, tokens_total, None, _js="(v)=>{ setStorage('otro',v) }")
-    resultadoFinal.change(fn=deduct, inputs=[resultadoFinal], outputs=[tokens_total], _js="(v)=>{ setStorage('tokens', v) }")
-
+    
     #resultadoFinal.change(None, text_input, resultadoFinal, js="(v)=>{ getStorage('text_input') }")
     btn = gr.Button("Enviar", icon="aiicon.png")
     payBtn = gr.Button("Buy Tokens", icon="aiicon.png", interactive = True, visible = True)
 
     btn.click(fn=predict, inputs=[text_input, tokens_total], outputs=[tokens_total, resultadoFinal, btn, payBtn])
+    resultadoFinal.change(fn=deduct, inputs=[resultadoFinal], outputs=[tokens_total], _js="(v)=>{ setStorage('tokens', v) }")
+
     payBtn.click(None, inputs=None, outputs=None, _js="(v)=>{ cleanCred('credused',0) }" )
     
     block.load(
